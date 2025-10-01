7751dd2b3393d4b6de5552f949e1f46dd5ab1c50
debalinamukh <mukhopadhyayd@msoe.edu>
2025-10-01 03:50:09 -0500

    Enhancement: update homepage/add /about route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello MSOE"
+    return "Hello Debalina"
 
 if __name__ == "__main__":
     # run the dev server on port 8080 so it matches your screenshot
